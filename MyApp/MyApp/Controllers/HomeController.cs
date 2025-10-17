bef2586cc7fb8525a9a8e98b0b4220e92e4ebc76
rgarcia <rgarcia@imperiascm.com>
2025-10-17 12:04:13 +0200

    feat\ routine creator page

--- MyApp/MyApp/Controllers/HomeController.cs
+++ MyApp/MyApp/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace MyApp.Controllers
             return View();
         }
 
+        public IActionResult RoutineCreator()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
